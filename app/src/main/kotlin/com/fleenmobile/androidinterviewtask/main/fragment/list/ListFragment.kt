f2f4c90ef6fb354812eb68b9467b4a7c3b027ac0
FleenMobile\Andrzej <fleenmobile@gmail.com>
2018-02-25 11:02:33 +0100

    Add base fragment

--- app/src/main/kotlin/com/fleenmobile/androidinterviewtask/main/fragment/list/ListFragment.kt
+++ app/src/main/kotlin/com/fleenmobile/androidinterviewtask/main/fragment/list/ListFragment.kt
@@ -1,12 +1,16 @@
 package com.fleenmobile.androidinterviewtask.main.fragment.list
 
-import android.support.v4.app.Fragment
+import com.fleenmobile.androidinterviewtask.BaseFragment
+import com.fleenmobile.androidinterviewtask.R
 import com.fleenmobile.androidinterviewtask.data.User
 
 /**
  * @author FleenMobile at 2018-02-25
  */
-class ListFragment : Fragment(), ListFragmentContract.View, ListFragmentContract.Router {
+class ListFragment : BaseFragment<ListFragmentContract.Presenter>(),
+        ListFragmentContract.View, ListFragmentContract.Router {
+
+    override val layoutId: Int = R.layout.fragment_list
 
     override fun showUsers(users: List<User>) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
